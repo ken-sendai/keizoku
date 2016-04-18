d4f7696766ee953a9e10bcfb2b4014ef4ac75501
blue <s.ken16@gmail.com>
2016-04-19 07:46:56 +0900

    productsテーブル作成

--- db/migrate/20160418223556_create_products.rb
+++ db/migrate/20160418223556_create_products.rb
@@ -1,8 +1,15 @@
 class CreateProducts < ActiveRecord::Migration
   def change
     create_table :products do |t|
-
-      t.timestamps null: false
+      t.text :name
+      t.text :start
+      t.date :finish
+      t.text :period
+      t.text :condition
+      t.integer :user_id
+      t.integer :count
+      t.integer :interval
+      t.timestamps
     end
   end
 end
